50ff9b4bef8756469b289f39f128186a0d072c26
Kibe <roygithara6@gmail.com>
2023-03-28 17:33:04 +0300

    created binary function

--- get_binary.c
+++ get_binary.c
@@ -3,27 +3,28 @@
 #include <stdio.h>
 
 /**
- * print_binary - Prints a binary to the screen
- * @l: va_list containing string to print
+ * print_every_binary - Prints a binary to the screen
+ * print_binary - Prints the binary representation of an unsigned int
+ * @num: number
  * Return: number of char printed
  */
 
- int print_every_binary(int num)
+int print_every_binary(int num)
 {
-    if (num > 1) {
-        print_every_binary(num / 2);
-    }
-    putchar((num % 2) ? '1' : '0');
-    return (1);
+	if (num > 1)
+	{
+	print_every_binary(num / 2);
+	}
+	putchar((num % 2) ? '1' : '0');
+	return (1);
 }
 
 int print_binary(va_list l)
 {
-    int chars_printed = 0;
-    unsigned int num = va_arg(l, int);
-    chars_printed += print_every_binary(num);
+	int chars_printed = 0;
+	unsigned int num = va_arg(l, int);
 
-    return (chars_printed);
+	chars_printed += print_every_binary(num);
+
+	return (chars_printed);
 }
-
-
